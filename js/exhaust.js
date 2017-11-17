e0b4827ff2b4eac59a13ccd82748ec27d674a0fa
mwatson <mattwat@gmail.com>
2017-11-17 17:21:22 -0500

    abstract exhaust particle

--- js/exhaust.js
+++ js/exhaust.js
@@ -2,8 +2,8 @@ class Exhaust extends Array {
 	update() {
 		for (let i = this.length-1; i >= 0; i--) {
 			if (this[i].alpha > 0) {
-				this[i].x -= this[i].dx + random();
-				this[i].y -= this[i].dy + random();
+				this[i].x -= this[i].dx + random(-1,1) / 2;
+				this[i].y -= this[i].dy + random(-1,1) / 2;
 				this[i].alpha -= 20*random();
 				this[i].r -= random();
 			} else {
@@ -17,5 +17,16 @@ class Exhaust extends Array {
 			fill(color(255, particle.alpha, 0, particle.alpha));
 			ellipse(particle.x, particle.y, particle.r, particle.r);
 		}
+	}
+}
+
+class Particle {
+	constructor(x, y, angle) {
+		this.x = x - 5*cos(angle);
+		this.y = y - 5*sin(angle);
+		this.dx = cos(angle);
+		this.dy = sin(angle);
+		this.r = 10;
+		this.alpha = 255;
 	}
 }
